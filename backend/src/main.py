c4a0bab46d3b0a289645e9780e817d4bc5f11fca
WalkerExplorer <139987437+WalkerExplorer@users.noreply.github.com>
2023-08-02 01:15:31 +0200

    Update main.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.logger import logger
 from classes import ModelData
-from model.classes import Code
+from classes import Code
 
 from generate import init_model
 app = FastAPI()
