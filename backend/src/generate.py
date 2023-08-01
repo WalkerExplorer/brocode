5ca7c2fe032be3116dbfd809c0a62f7bb2afcd91
WalkerExplorer <139987437+WalkerExplorer@users.noreply.github.com>
2023-08-02 00:39:57 +0200

    Update generate.py

--- backend/src/generate.py
+++ backend/src/generate.py
@@ -1,5 +1,4 @@
-from .model.codegen import CodeGenerationPipeline
-
+from brocode.backend.src.model.codegen import CodeGenerationPipeline
 from .classes import ModelData
 
 def init_model(model_data = ModelData):
